1e7700b4cb8d5ed84426eff3f02f17cbf6cdd6d0
agustinquetglas <agustinquetglas19@gmail.com>
2025-08-26 11:25:12 -0300

    Agrego clases, interfaces y comienzo a codear

---
+++ src/main/java/com/mycompany/app/Disparo.java
@@ -0,0 +1,6 @@
+package com.mycompany.app;
+
+public interface Disparo {
+   int daño_base = 100;
+   void Disparar(Recibirdisparo objetivo);
+}
---
+++ src/main/java/com/mycompany/app/Recibirdisparo.java
@@ -0,0 +1,6 @@
+package com.mycompany.app;
+
+public interface Recibirdisparo {
+    void recibirDisparo(Disparo disparo);
+    boolean estaVivo();
+}
